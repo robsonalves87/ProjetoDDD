4e2b4696967601bd9ecf351dc637c5566936c3e0
Robson Alves <robsonalves87@live.com>
2020-02-01 19:50:44 -0300

    Adicionando a primeira migração

---
+++ ProjetoDDD/src/ProjetoDDD.Infrastructure.Data/Migrations/20200201224844_Inicial.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoDDD.Infrastructure.Data.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false),
+                    Sobrenome = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false),
+                    CPF = table.Column<string>(type: "varchar(11)", maxLength: 11, nullable: false),
+                    RG = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false),
+                    Email = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime", nullable: false),
+                    DataInclusao = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: false),
+                    Valor = table.Column<decimal>(nullable: false),
+                    DataInclusao = table.Column<DateTime>(nullable: false),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
