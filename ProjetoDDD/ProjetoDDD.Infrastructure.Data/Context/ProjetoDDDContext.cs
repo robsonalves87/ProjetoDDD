05bcb387ced9802ce0c4c201e186715424a9c034
Robson Alves <robsonalves87@live.com>
2020-02-01 10:14:57 -0300

    Commit 2

--- ProjetoDDD/ProjetoDDD.Infrastructure.Data/Context/ProjetoDDDContext.cs
+++ ProjetoDDD/ProjetoDDD.Infrastructure.Data/Context/ProjetoDDDContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetoDDD.Infrastructure.Data.Context
 {
-    public class ProjetoDDDContext : DBContext
+    public class ProjetoDDDContext : DbContext
     {
     }
 }
